af236b937e35bb7b11173a37202faec201012334
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-08-30 15:36:36 -0500

    update_project fix code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-'''
-Module to list all states from database hbtn_0e_0_usa
-'''
+"""Module to list all states from database hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
@@ -9,9 +7,9 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-
 
     sql = "SELECT * FROM states ORDER BY states.id ASC"
 
